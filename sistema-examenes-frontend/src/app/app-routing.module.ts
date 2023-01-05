d910e05ed1dc60a314eea41673fde2bc40030898
erick godoy <erickalexis23@hotmail.com>
2023-01-05 03:56:41 +0300

    avance 04-01

--- sistema-examenes-frontend/src/app/app-routing.module.ts
+++ sistema-examenes-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+    pathMatch:'full' 
+   },
+  {
+   path:'signup',
+   component:SignupComponent,
+   pathMatch:'full' 
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
