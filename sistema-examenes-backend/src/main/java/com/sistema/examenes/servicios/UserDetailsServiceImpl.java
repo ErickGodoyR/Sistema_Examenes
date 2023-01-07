a1ac5e5fde9d64f5e3ca66fd80f353fa7e7b2b1d
ErickGodoy <98168370+ErickGodoyR@users.noreply.github.com>
2023-01-07 10:49:45 -0300

    actualización jwt

---
+++ sistema-examenes-backend/src/main/java/com/sistema/examenes/servicios/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sistema.examenes.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sistema.examenes.entidades.Usuario;
+import com.sistema.examenes.repositorio.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = this.usuarioRepository.findByUsername(username);
+		if(usuario == null){
+			throw new UsernameNotFoundException("Usuario no existe");
+		}
+		return usuario;
+	}
+
+}
